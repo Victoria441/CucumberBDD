daded68935e37a43e5c9ae4dfaf599b5a4f84d78
Victoria441 <v.abudinenvelarde@gmail.com>
2020-08-22 14:36:01 -0400

    update BDD

--- src/test/java/step_implementation/CommonPageImpl.java
+++ src/test/java/step_implementation/CommonPageImpl.java
@@ -3,14 +3,14 @@ package step_implementation;
 import utils.Driver;
 
 import java.util.Set;
+    public class CommonPageImpl {
 
-public class CommonPageImpl {
-    public  void switchToWindow(String currentPageID) {
-        Set<String> set = Driver.getDriver().getWindowHandles();
-
-        for(String window : set) {
-            if(!window.equalsIgnoreCase(currentPageID))
-                Driver.getDriver()
+        public void switchToWindow(String currentPageID){
+            Set<String> set = Driver.getDriver().getWindowHandles();
+            for (String window: set){
+                if (!window.equalsIgnoreCase(currentPageID))
+                    Driver.getDriver().switchTo().window(window);
+            }
         }
+
     }
-}
--- src/test/java/step_implementation/UserMgtPageImpl.java
+++ src/test/java/step_implementation/UserMgtPageImpl.java
@@ -1,4 +1,30 @@
 package step_implementation;
 
+import pages.UserMgtPage;
+import utils.SeleniumUtils;
+
 public class UserMgtPageImpl {
+    public void fillRegistrationForm(Map<String, String> map){
+        UserMgtPage userMgtPage = new UserMgtPage();
+        for(String key: map.keySet()) {
+            switch (key.toLowerCase()) {
+                case "firstname":
+                    SeleniumUtils.sendKeys(userMgtPage.firstnameField, map.get(key));
+                    break;
+                case "lastname":
+                    SeleniumUtils.sendKeys(userMgtPage.lastnameField, map.get(key));
+                    break;
+                case "phone number":
+                    SeleniumUtils.sendKeys(userMgtPage.phoneField, map.get(key));
+                    break;
+                case "email":
+                    SeleniumUtils.sendKeys(userMgtPage.emailField, map.get(key));
+                    break;
+                case "role":
+                    userMgtPage.selectRole(map.get(key));
+                default:
+                    System.out.println("Invalid field type");
+            }
+        }
+    }
 }
